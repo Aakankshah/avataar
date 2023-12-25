20ffaa4b51dd9a7141b312e443aea2b6d9bf8882
Aakankshah <aakankshashah2502@gmail.com>
2023-12-25 23:28:32 +0530

    more section

--- navbar.js
+++ navbar.js
@@ -1,16 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const navbar = document.querySelector('.navbar');
-    const moreDropdown = document.querySelector('.more-dropdown-content');
-    const Dropdown = document.querySelector('.more-dropdown');
-    const bodyWidth = document.body.clientWidth;
-    console.log(`${bodyWidth}`);
-  
-    let maxVisibleItems = calculateMaxVisibleItems(); // Initial calculation
-  
-    function calculateMaxVisibleItems() {
-      // Calculate the maximum visible items based on the window width
+document.addEventListener  ("DOMContentLoaded", function () {
+   
+    function updateNavbar() {
+     
+      /*reset();*/
+      const navbar = document.querySelector('.navbar');
+      let moreDropdown = document.querySelector('.more-dropdown-content');
+      const Dropdown = document.querySelector('.more-dropdown');
+      const bodyWidth = document.body.clientWidth;
       
-      
+      if (bodyWidth > 856) {
+        Dropdown.style.display = 'none';
+      } 
+      else {
+        Dropdown.style.display = 'inline-flex';
+      }
+
+      let maxVisibleItems = calculateMaxVisibleItems(bodyWidth);
+      const navbarItems = navbar.querySelectorAll('a');
+      let hiddenItems = [];
+      navbarItems.forEach((item, index) => 
+      {
+        if (index >= maxVisibleItems) {
+          hiddenItems.push(item.cloneNode(true));
+          item.style.display = 'none';
+
+        } 
+        else 
+        {
+          item.style.display = 'inline-flex';
+        }
+    });
+    reset();
+      hiddenItems.forEach((hiddenItem) => {
+        moreDropdown.appendChild(hiddenItem);
+      });
+    }
+
+    function calculateMaxVisibleItems(bodyWidth) {
+      // Calculate the maximum visible items based on the window width      
       let visibleItems = 6;
       if (bodyWidth <= 856 && bodyWidth>805) {
         visibleItems = 5;
@@ -24,43 +51,24 @@ document.addEventListener("DOMContentLoaded", function () {
       if (bodyWidth <= 595 && bodyWidth>500) {
         visibleItems = 2;
       } 
-
+      
       return visibleItems;
     }
 
-    
-   
-
+    function reset()
+    {
+      let dropdown = document.querySelector('.more-dropdown-content');
+     
+    while(dropdown.firstChild)
+    {
+      dropdown.removeChild(dropdown.firstChild);
+    }
   
-    function updateNavbar() {
-      if (bodyWidth > 856) {
-        Dropdown.style.display = 'none';
-      } 
-      else {
-        Dropdown.style.display = 'inline-flex';
-      }
-  
-      const navbarItems = navbar.querySelectorAll('a');
-      const hiddenItems = [];
-  
-      navbarItems.forEach((item, index) => {
-        if (index >= maxVisibleItems) {
-          hiddenItems.push(item.cloneNode(true));
-          item.style.display = 'none';
-        } 
-        else {
-          item.style.display = 'inline-flex';
-        }
-      });
-  
-      moreDropdown.innerHTML = ''; // Clear items in the "More" dropdown
-      hiddenItems.forEach((hiddenItem) => {
-      moreDropdown.appendChild(hiddenItem);
-      });
     }
 
+    
     window.addEventListener("resize", function () 
-    {
+    { 
       updateNavbar();
     });
 
